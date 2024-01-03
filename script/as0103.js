227787c4ae4e48b0457b3e42594049a631c820fc
won <won00yeom@naver.com>
2024-01-03 11:57:46 +0900

    체중계 만들기

---
+++ script/as0103.js
@@ -0,0 +1,13 @@
+let height = document.querySelector('#height')
+let weight = document.querySelector('#weight')
+let goals = document.querySelector('#goals')
+const result = document.querySelector('#result')
+
+// 적정체중 = (본인신장-100)*0.9
+
+result.addEventListener('click', function(){
+    const normal_w = (Number(height.value)-100)*0.9;
+    const over_w = weight.value - Number(normal_w);
+    console.log(normal_w)
+    goals.value = `적정체중은 ${normal_w}kg이며 ${over_w}kg 초과되셨습니다.`;
+})
