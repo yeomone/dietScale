0af50c2c9ad8733d2ef36f6da166c2eba8db7561
won <won00yeom@naver.com>
2024-01-03 14:23:43 +0900

    체중계

--- script/as0103.js
+++ script/as0103.js
@@ -1,5 +1,6 @@
 let height = document.querySelector('#height')
 let weight = document.querySelector('#weight')
+let goals_weight = document.querySelector('#goals_weight')
 let goals = document.querySelector('#goals')
 const result = document.querySelector('#result')
 
@@ -9,5 +10,6 @@ result.addEventListener('click', function(){
     const normal_w = (Number(height.value)-100)*0.9;
     const over_w = weight.value - Number(normal_w);
     console.log(normal_w)
-    goals.value = `적정체중은 ${normal_w}kg이며 ${over_w}kg 초과되셨습니다.`;
+    goals_weight.value = normal_w;
+    goals.value = `적정체중은 ${normal_w}kg이며 ${over_w}kg 초과되셨습니다`;
 })
